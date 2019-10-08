24d6642af896bf986f8901ea3484d779dcb56ca2
BaronLR <mattfalkner49@gmail.com>
2019-10-07 20:29:15 -0400

    click and event to view its details, check in or get directions, includes hashmap datastructure for pins

---
+++ android/app/src/main/java/com/example/poppin/BottomSheetFragment.java
@@ -0,0 +1,29 @@
+package com.example.poppin;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+    public BottomSheetFragment() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
+    }
+}
